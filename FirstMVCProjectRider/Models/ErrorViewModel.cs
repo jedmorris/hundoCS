171c798b117019e8cd91596c54be00cd6856554a
jedmorris <jemorris110@gmail.com>
2022-01-25 14:48:49 -0800

    created a funcitonal version of hundo

---
+++ FirstMVCProjectRider/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FirstMVCProjectRider.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
